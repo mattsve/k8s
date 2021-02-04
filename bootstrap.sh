03d8a9174932d4621ccc675eaa63a3a70c02ab83
Mattias Svensson <mattsve@users.noreply.github.com>
2021-02-05 00:42:43 +0100

    Update node red secret

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,5 @@
 kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.12.5/controller.yaml
 kubectl apply -f ~/master.key
 kubectl delete pod -n kube-system -l name=sealed-secrets-controller
-kubectl apply -k argocd/
+kustomize build argocd/ | kubectl apply -f -
 kubectl apply -f app-of-apps.yaml
