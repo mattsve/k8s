e8bd1ab449effd089b0e434adf2353f7947bbe95
Mattias Svensson <mattsve@users.noreply.github.com>
2021-02-05 10:10:15 +0100

    Disable key renewal for sealed secrets

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.12.5/controller.yaml
+kubectl apply -k sealed-secrets/
 kubectl apply -f ~/master.key
 kubectl delete pod -n kube-system -l name=sealed-secrets-controller
 kustomize build argocd/ | kubectl apply -f -
