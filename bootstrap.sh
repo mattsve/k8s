39320bb12fac6ad1fcd9ff18a367f61d5c9ebefe
Mattias Svensson <mattsve@users.noreply.github.com>
2023-05-12 14:03:37 +0200

    Move 1password to onepassword

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 op connect server create k8s
-op connect token create k8s-token --server k8s --vaults k8s,r > k8s-token.txt 
+base64 -w 0 1password-credentials.json > 1password-credentials.json.b64
+rm 1password-credentials.json && mv 1password-credentials.json.b64 1password-credentials.json
 kubectl apply -f 1password/templates/namespace.yaml
-kubectl create secret generic op-credentials --from-file=1password-credentials.json --from-file=token=./k8s-token.txt --namespace=1password --dry-run=client -o yaml | kubectl apply -f -
+kubectl create secret generic op-credentials --from-file=1password-credentials.json --from-literal=token=$(op create connect token k8s op-k8s-operator --vault k8s,r) --namespace=1password --dry-run=client -o yaml | kubectl apply -f -
 kubectl apply -k sealed-secrets/
 kubectl apply -f ~/master.key
 kubectl delete pod -n kube-system -l name=sealed-secrets-controller
